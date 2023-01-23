fcfd7c56db3b433218a0bd1bb2e6c55d28aa3b8b
ViktorijaVer <viktorija.verbickaite@gmail.com>
2023-01-23 03:02:47 +0200

    init: webpack was installed

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+const age = 23;
+console.log(age);
+
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ]
+    },
+
+    devtool: false,
+    
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+}
